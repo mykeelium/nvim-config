510376bcf21a988af3d5df38b87a5a119fd55488
Michael Cuomo <mcuomo777@gmail.com>
2025-05-02 14:14:02 -0400

    Update Config for which-key, create voidlight configuration

---
+++ voidlight/lua/voidlight/init.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " " -- Set leader key before Lazy
+
+require("voidlight.lazy_init")
+require("voidlight.file_explorer")
+require("voidlight.remap")
+require("voidlight.set")
+require('mini.icons').setup()
