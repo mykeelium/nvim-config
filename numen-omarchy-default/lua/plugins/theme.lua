9b2db76e1c088b58b49ecfe59c8b57cd9282896c
Michael Cuomo <mcuomo777@gmail.com>
2025-12-17 18:08:11 -0500

    add numen and default

---
+++ numen-omarchy-default/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/mcuomo/.config/omarchy/current/theme/neovim.lua
---
+++ numen/lua/arcana/init.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " " -- Set leader key before Lazy
+
+require("arcana.lazy_init")
+require("arcana.file_explorer")
+require("arcana.remap")
+require("arcana.set")
+require('mini.icons').setup()
