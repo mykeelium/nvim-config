3233492e6550fa73de3bff10c17bfcfd65d5af4c
Michael Cuomo <mcuomo777@gmail.com>
2026-01-24 16:43:37 -0500

    fix: add state and lockfile for pack lazy

--- pack/lua/machine/lazy_init.lua
+++ pack/lua/machine/lazy_init.lua
@@ -15,4 +15,6 @@ require("lazy").setup({
   spec = "machine.lazy",
   change_detection = { notify = false },
   root = "/usr/share/nvim/plugins",
+  state = "/var/lib/nvim/lazy",
+  lockfile = "/usr/share/nvim/lazy-lock.json",
 })
