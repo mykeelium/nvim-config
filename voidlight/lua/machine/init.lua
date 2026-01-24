2ea0d395cdfd34af535ce045cac71d6a56aecc41
Michael Cuomo <mcuomo777@gmail.com>
2026-01-24 11:41:58 -0500

    chore: move machine specific directory to machine for easier comparison

--- voidlight/lua/machine/init.lua
+++ voidlight/lua/machine/init.lua
@@ -1,10 +1,10 @@
 vim.g.mapleader = " " -- Set leader key before Lazy
 vim.g.python3_host_prog = vim.fn.expand("/home/mcuomo/.pyenv/versions/3.14.2/bin/python")
 
-require("voidlight.lazy_init")
-require("voidlight.file_explorer")
-require("voidlight.remap")
-require("voidlight.set")
-require("voidlight.autocmds")
+require("machine.lazy_init")
+require("machine.file_explorer")
+require("machine.remap")
+require("machine.set")
+require("machine.autocmds")
 require("config.diagnostics")
 require('mini.icons').setup()
